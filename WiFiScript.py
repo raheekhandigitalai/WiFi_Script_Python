d6b4d4fa4e08cadd8018fe93a9474162ca33cab2
Rahee Khan <RKhan@rahee-khan.lan>
2021-12-27 16:37:23 -0500

    [#14] - Minor cleanup

--- WiFiScript.py
+++ WiFiScript.py
@@ -26,13 +26,13 @@ class SampleTestCase(unittest.TestCase):
     # def test_2(self):
         # print(get_device_id('56793ec400fe2121df8a6341591cbd25b7c26c70'))
 
-    capabilities['testName'] = 'pythonTest'
+    capabilities['testName'] = 'Webhook cleanup'
     capabilities['accessKey'] = '%s' % config.get('seetest_authorization', 'access_key_cleanup')
     capabilities['udid'] = '%s' % uid
     capabilities['platformName'] = 'iOS'
     capabilities['autoDismissAlerts'] = True
     capabilities['releaseDevice'] = False
-    capabilities['newCommandTimeout'] = 60
+    # capabilities['newCommandTimeout'] = 60
     capabilities['bundleId'] = 'com.apple.Preferences'
 
     def setUp(self):
@@ -41,17 +41,17 @@ class SampleTestCase(unittest.TestCase):
     def test_1(self):
         device_udid = self.driver.capabilities['udid']
         device_id = get_device_id(device_udid)
-        wifi_label = self.driver.find_element(By.XPATH, "(//*[@id='Wi-Fi']//XCUIElementTypeStaticText)[2]")
+        wifi_label = self.driver.find_element(By.XPATH, "(//*[@id='Wi-Fi']//XCUIElementTypeStaticText)[2]").text
 
         # Check if the desired wifi name is present in the connected wifi
-        if config.get('wifi', 'wifi_name') in wifi_label.text:
-            print('Connected to correct WiFi: %s' % wifi_label.text)
+        if config.get('wifi', 'wifi_name') in wifi_label:
+            print('Connected to correct WiFi: %s' % wifi_label)
             # remove all device tags
             remove_all_device_tags(device_id)
             # add custom device tag
             add_device_tag(device_id, config.get('tags', 'good_tag_value'))
         else:
-            print('Not Connected to correct WiFi: %s' % wifi_label.text)
+            print('Not Connected to correct WiFi: %s' % wifi_label)
             # remove all device tags
             remove_all_device_tags(device_id)
             # add custom device tag
