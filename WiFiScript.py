7d8b921f6d584173a83b5eb4e436c70f174ae3d0
Rahee Khan <RKhan@rahee-khan.lan>
2021-12-27 16:30:11 -0500

    [#14] - Cleaned up code and removed hardcoded values in script

--- WiFiScript.py
+++ WiFiScript.py
@@ -2,49 +2,23 @@ import os
 import unittest
 import configparser
 
-from selenium.webdriver import DesiredCapabilities
-
 from APIs import remove_all_device_tags
 from APIs import add_device_tag
 from APIs import get_device_id
 from APIs import finish_cleanup_state
 from appium import webdriver
 from selenium.webdriver.common.by import By
-
-access_key_admin = 'eyJ4cC51Ijo3MzU0MjQsInhwLnAiOjIsInhwLm0iOiJNVFUzT0RZd016ZzFOek16TVEiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4OTM5NjM4NTcsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.GP0hK0o0j2WEKt-J0aXsVbu1tmt-PhWUryqluokszJk'
-
-wifi_name = "ECustomers_ALL_Legacy"
-bad_tag_value = 'BadWiFi'
-good_tag_value = 'GoodWiFi'
+from selenium.webdriver import DesiredCapabilities
 
 status = 'failed'
 
 uid = os.getenv("deviceID")
-# uid = '56793ec400fe2121df8a6341591cbd25b7c26c70'
 os = os.getenv("deviceOS")
 
 capabilities = DesiredCapabilities.IPHONE
 
 config = configparser.ConfigParser()
 config.read('config.properties')
-# device_name = os.getenv("deviceName")
-# os_version = os.getenv("osVersion")
-# device_model = os.getenv("deviceModel")
-# device_manufacturer = os.getenv("deviceManufacturer")
-# device_category = os.getenv("deviceCategory")
-# username = os.getenv("username")
-# user_project = os.getenv("userProject")
-
-# desired_caps = dict(
-#     testName="PythonTest",
-#     accessKey="eyJ4cC51Ijo3MzU0MjQsInhwLnAiOjIsInhwLm0iOiJNVFUzT0RZd016ZzFOek16TVEiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4OTM5NjM4NTcsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.GP0hK0o0j2WEKt-J0aXsVbu1tmt-PhWUryqluokszJk",
-#     deviceQuery="@serialnumber='56793ec400fe2121df8a6341591cbd25b7c26c70'",
-#     # deviceQuery="@serialnumber='%s'" % uid,
-#     platformName="iOS",
-#     autoDismissAlerts=True,
-#     releaseDevice=False,
-#     bundleId="com.apple.Preferences"
-# )
 
 
 class SampleTestCase(unittest.TestCase):
@@ -53,7 +27,6 @@ class SampleTestCase(unittest.TestCase):
         # print(get_device_id('56793ec400fe2121df8a6341591cbd25b7c26c70'))
 
     capabilities['testName'] = 'pythonTest'
-    # capabilities['accessKey'] = '%s' % config.get('seetest_authorization', 'access_key_admin')
     capabilities['accessKey'] = '%s' % config.get('seetest_authorization', 'access_key_cleanup')
     capabilities['udid'] = '%s' % uid
     capabilities['platformName'] = 'iOS'
@@ -67,26 +40,25 @@ class SampleTestCase(unittest.TestCase):
 
     def test_1(self):
         device_udid = self.driver.capabilities['udid']
-        # device_udid = self.driver.caps['udid'] // works in selenium4 / appiumclient 2.1.0
         device_id = get_device_id(device_udid)
         wifi_label = self.driver.find_element(By.XPATH, "(//*[@id='Wi-Fi']//XCUIElementTypeStaticText)[2]")
 
-        if wifi_name in wifi_label.text:
-            print('found')
+        # Check if the desired wifi name is present in the connected wifi
+        if config.get('wifi', 'wifi_name') in wifi_label.text:
+            print('Connected to correct WiFi: %s' % wifi_label.text)
             # remove all device tags
             remove_all_device_tags(device_id)
             # add custom device tag
-            add_device_tag(device_id, good_tag_value)
+            add_device_tag(device_id, config.get('tags', 'good_tag_value'))
         else:
-            print('not found')
+            print('Not Connected to correct WiFi: %s' % wifi_label.text)
             # remove all device tags
             remove_all_device_tags(device_id)
             # add custom device tag
-            add_device_tag(device_id, bad_tag_value)
+            add_device_tag(device_id, config.get('tags', 'bad_tag_value'))
 
     def tearDown(self):
         status = 'passed'
-        # print(status)
         self.driver.quit()
         finish_cleanup_state(uid, status)
 
